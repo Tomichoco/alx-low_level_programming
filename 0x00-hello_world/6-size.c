b350e86b2c1fbd77507d7d2211d5befd86d2c3b1
root <root@DESKTOP-HDFSL3U.localdomain>
2022-09-08 15:08:19 +0100

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,11 @@
  */
 int main(void)
 {
-	int i;
-	char j;
-	long int x;
-	long long int y;
-	float z;
-	printf("Size of a char is: %lu byte(s).\n", (unsigned long)sizeof(j));
-       		 printf("Size of a int is: %lu byte(s).\n", (unsigned long)sizeof(i));
-			printf("Size of a long int is: %lu byte(s).\n", (unsigned long)sizeof(x));
-				printf("Size of a long long int is: %lu byte(s).\n", (unsigned long)sizeof(y));
-					printf("Size of a float is: %lu byte(s).\n", (unsigned long)sizeof(z));
-						return (0);
-}	
+		printf("Size of a char is: %lu byte(s).\n", (unsigned long)sizeof(char));
+       			 printf("Size of a int is: %lu byte(s).\n", (unsigned long)sizeof(int));
+				printf("Size of a long int is: %lu byte(s).\n", (unsigned long)sizeof(long int));
+					printf("Size of a long long int is: %lu byte(s).\n", (unsigned long)sizeof(long long int));
+						printf("Size of a float is: %lu byte(s).\n", (unsigned long)sizeof(float));
+							return (0);
+}							
 
